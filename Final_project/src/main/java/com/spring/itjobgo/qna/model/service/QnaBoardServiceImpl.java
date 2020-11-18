4428024c58e250ce7290f8e2657b037d79569a5a
eggbbang <khj190936@gmail.com>
2020-11-18 23:11:32 +0900

    1118 hyeonju

--- Final_project/src/main/java/com/spring/itjobgo/qna/model/service/QnaBoardServiceImpl.java
+++ Final_project/src/main/java/com/spring/itjobgo/qna/model/service/QnaBoardServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.itjobgo.qna.model.dao.QnaBoardDao;
+import com.spring.itjobgo.qna.model.vo.QB_ATTACHMENT;
 import com.spring.itjobgo.qna.model.vo.QnaBoard;
 
 	@Service
@@ -22,6 +23,30 @@ import com.spring.itjobgo.qna.model.vo.QnaBoard;
 	public List<QnaBoard> selectQnaBoard(){
 		
 		return dao.selectQnaBoard(session);
+		
+		
+	}
+
+	//qna게시판 글쓰기
+	@Override
+	public int insertQnaBoard(QnaBoard qb, List<QB_ATTACHMENT> files) {
+
+		int result=dao.insertQnaBoard(session,qb);
+		
+		if(result==0) throw new RuntimeException("입력오류");
+		if(result>0) {
+			if(!files.isEmpty()) {
+				for(QB_ATTACHMENT file:files) {
+					
+					result=dao.insertAttachment(session,file);
+					
+					if(result==0) throw new RuntimeException("입력오류");
+					
+				}
+			}
+		}
+		
+		return result;
 	}
 
 
