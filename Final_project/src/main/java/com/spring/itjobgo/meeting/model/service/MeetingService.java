415bb90a1d45e19db78f4d7a3a140e086cdc278d
DK2554 <ldu0215@gmail.com>
2020-11-19 15:37:30 +0900

    모임C

--- Final_project/src/main/java/com/spring/itjobgo/meeting/model/service/MeetingService.java
+++ Final_project/src/main/java/com/spring/itjobgo/meeting/model/service/MeetingService.java
@@ -1,5 +1,13 @@
 package com.spring.itjobgo.meeting.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.spring.itjobgo.meeting.model.vo.Mattachment;
+
+
 public interface MeetingService {
+
+	int insertMboard(Map param, List<Mattachment> files);
 
 }
