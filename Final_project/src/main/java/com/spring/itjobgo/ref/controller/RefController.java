95ee32152f30e5a66e4080e0f61b6eb4016418eb
eggbbang <khj190936@gmail.com>
2020-12-11 15:57:14 +0900

    reference 조회

--- Final_project/src/main/java/com/spring/itjobgo/ref/controller/RefController.java
+++ Final_project/src/main/java/com/spring/itjobgo/ref/controller/RefController.java
@@ -1,7 +1,9 @@
 package com.spring.itjobgo.ref.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,18 +11,25 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.spring.itjobgo.ItNews.model.vo.ItnewsAttachment;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.spring.itjobgo.ref.model.service.RefService;
+import com.spring.itjobgo.ref.model.vo.REF_SITE;
 import com.spring.itjobgo.ref.model.vo.REF_SITE_ATTACHMENT;
 
 @RestController
@@ -75,5 +84,45 @@ public class RefController {
 		}
 		return msg;
 	}
+	
+	//리스트 불러오기(조회)
+	@RequestMapping(value="ref/selectsite",method = RequestMethod.GET)
+	public List<REF_SITE> selectList()throws JsonMappingException,JsonGenerationException,IOException{
+		List<REF_SITE> list=service.selectList();
+					logger.debug("list 불러오기 성공");
+					logger.debug(list.toString());
+			return list;
+	}
+	
+	//리스트 이미지 불러오기
+	@RequestMapping(value="ref/selectsiteImg",method=RequestMethod.GET,produces=MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] selectImage(@PathVariable int no, HttpServletRequest request, HttpServletResponse res)throws Exception{
+		logger.debug("이미지요청~");
+		//받아온 번호로 해당 첨부파일 db가서 받아오는 로직수행
+		REF_SITE_ATTACHMENT mt=service.selectImage(no);
+		
+		logger.debug(mt.toString());
+		//파일경로
+		String realFile = request.getServletContext().getRealPath("/resources/upload/refsite");
+		//파일이름
+		String fileNm = mt.getRenamedfilename();
+		//파일확장자
+		String ext = fileNm.substring(fileNm.lastIndexOf(".")+1);
+		String image=realFile+"//"+fileNm;
+		
+		logger.debug("realFile : "+realFile+"fileNm : "+fileNm+"ext : "+ext);
+		logger.debug(realFile+"\\"+fileNm);
+		
+		InputStream in = new FileInputStream(image);
+		byte[] imageByteArray=IOUtils.toByteArray(in);
+		in.close();
+		
+		return imageByteArray;
+	}
+	
+	
+	
+	
+	
 	
 }
--- Final_project/src/main/java/com/spring/itjobgo/ref/model/dao/RefDao.java
+++ Final_project/src/main/java/com/spring/itjobgo/ref/model/dao/RefDao.java
@@ -1,9 +1,11 @@
 package com.spring.itjobgo.ref.model.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
+import com.spring.itjobgo.ref.model.vo.REF_SITE;
 import com.spring.itjobgo.ref.model.vo.REF_SITE_ATTACHMENT;
 
 public interface RefDao {
@@ -14,5 +16,10 @@ public interface RefDao {
 	//글작성(첨부파일)
 	int insertAttachment(SqlSessionTemplate session, REF_SITE_ATTACHMENT file);
 	
-
+	//리스트 불러오기(조회)
+	List<REF_SITE> selectList(SqlSessionTemplate session);
+	
+	//이미지 불러오기
+	REF_SITE_ATTACHMENT selectImage(SqlSessionTemplate session, int no);
+	
 }
